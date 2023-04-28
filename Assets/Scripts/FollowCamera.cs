2e412b5891e44e0604e351049b6df4b7e1874d11
ironlionm4n <ironlionm4n@gmail.com>
2023-04-28 12:08:15 -0400

    hold left mouse to move to point continuously

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void Update()
+    {
+        transform.position = target.position;
+    }
+}
--- Assets/Scripts/PlayerMovement/NavMeshAgentMover.cs
+++ Assets/Scripts/PlayerMovement/NavMeshAgentMover.cs
@@ -1,21 +1,74 @@
 using System;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class NavMeshAgentMover : MonoBehaviour
 {
     #region Variables
-    [SerializeField] private Transform target;
 
+    [SerializeField] private InputActionAsset playerMoveInputActionAsset;
+    [SerializeField] private Animator playerAnimator;
     private NavMeshAgent _playerAgent;
+    private Ray _lastRay;
+    private InputAction _leftClickAction;
+    private InputActionMap _playerMoveActionMap;
+    private static readonly int MovementSpeed = Animator.StringToHash("MoveSpeed");
+
     #endregion
-    
+
     #region UnityMethods
+
+    private void Awake()
+    {
+        _playerMoveActionMap = playerMoveInputActionAsset.FindActionMap("PlayerMovement");
+        _leftClickAction = _playerMoveActionMap.FindAction("ClickToMove");
+        _playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnEnable()
+    {
+        _leftClickAction.Enable();
+    }
 
     private void Start()
     {
-        _playerAgent = GetComponent<NavMeshAgent>();
-        _playerAgent.SetDestination(target.position);
+   
+    }
+
+    private void Update()
+    {
+        Debug.Log(_leftClickAction.phase);
+        if (_leftClickAction.phase == InputActionPhase.Performed)
+        {
+            if (Camera.main != null) MoveToCursor();
+        }
+        
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        // Convert from global to local because the NavMeshAgent velocity is in world space,
+        // Animator parameter needs the local velocity to blend correctly
+        var localVelocity = transform.InverseTransformDirection(_playerAgent.velocity);
+        playerAnimator.SetFloat(MovementSpeed, localVelocity.z);
+    }
+
+    private void OnDisable()
+    {
+        _leftClickAction.Disable();
+    }
+
+    #endregion
+
+    #region CustomFunctions
+    
+    private void MoveToCursor()
+    {
+        var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit raycastHit;
+        if (Physics.Raycast(ray, out raycastHit)) _playerAgent.SetDestination(raycastHit.point);
     }
 
     #endregion
