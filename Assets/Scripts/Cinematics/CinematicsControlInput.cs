95791029e6a7a1969f43063cf3b9e56c8a166599
ironlionm4n <ironlionm4n@gmail.com>
2023-05-04 09:03:41 -0400

    second scene, portal for loading new scenes

--- Assets/Scripts/Cinematics/CinematicsControlInput.cs
+++ Assets/Scripts/Cinematics/CinematicsControlInput.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.Playables;
 using RPG.Core;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace RPG.Cinematics
 {
     public class CinematicsControlInput : MonoBehaviour
     {
+        [SerializeField] private InputActionAsset playerActionAsset;
         private PlayableDirector _playableDirector;
+        private InputAction _playerInput;
+        private GameObject _player;
+        private const string PrimaryButton = "PrimaryButton";
 
         private void Awake()
         {
             _playableDirector = GetComponent<PlayableDirector>();
+        }
+
+        private void Start()
+        {
+            _playerInput = playerActionAsset.FindAction(PrimaryButton);
+            _player = GameObject.FindGameObjectWithTag("Player");
         }
 
         private void OnEnable()
@@ -29,14 +42,16 @@ namespace RPG.Cinematics
         {
             // when cinematic starts disable control
             Debug.Log("Started, disabling control");
-            var player = GameObject.FindGameObjectWithTag("Player");
-            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            _player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            _playerInput.Disable();
+            
         }
 
         private void EnableControl(PlayableDirector playableDirector)
         {
             // when cinematic ends enable control
             Debug.Log("Stopped, enabling control");
+            _playerInput.Enable();
         }
     }
 }
