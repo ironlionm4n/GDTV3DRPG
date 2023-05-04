908fbd1c7ce37437583b003a26f05eb90a8af3a1
ironlionm4n <ironlionm4n@gmail.com>
2023-05-04 12:18:34 -0400

    start of portal fading transition, post volume prefab

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,8 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField] private int sceneIndex;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(SceneLoadTransition());
+        }
+    }
+
+    private IEnumerator SceneLoadTransition()
+    {
+        yield return SceneManager.LoadSceneAsync(sceneIndex);
+        Destroy(gameObject);
+    }
 }
