7c980a5fab1e8f6ebc44213ab3fa81634d61f272
ironlionm4n <ironlionm4n@gmail.com>
2023-05-13 10:11:42 -0400

    homing projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed;
+    [SerializeField] private bool isHoming;
     private Health _target;
     private CapsuleCollider _targetCapsuleCollider;
     private float _damage;
@@ -13,15 +14,20 @@ public class Projectile : MonoBehaviour
     {
         if (_target != null)
         {
-            transform.LookAt(GetAimLocation());
+            if (isHoming && !_target.HasDied) LookAtTargetAimLocation();
             transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
         }
+    }
+
+    private void LookAtTargetAimLocation()
+    {
+        transform.LookAt(GetAimLocation());
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Health>() != _target) return;
-        
+        if (_target.HasDied) return;
         _target.TakeDamage(_damage);
         Destroy(gameObject);
     }
@@ -31,10 +37,11 @@ public class Projectile : MonoBehaviour
         _target = target;
         _targetCapsuleCollider = target.GetComponent<CapsuleCollider>();
         _damage = damage;
+        LookAtTargetAimLocation();
     }
 
     private Vector3 GetAimLocation()
     {
-        return _target.transform.position + (Vector3.up * _targetCapsuleCollider.height / 1.75f);
+        return _target.transform.position + Vector3.up * _targetCapsuleCollider.height / 1.75f;
     }
 }
