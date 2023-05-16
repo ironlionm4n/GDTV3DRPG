420a7a21ee590e8d5792a9d0d8d3fc09681407c9
ironlionm4n <ironlionm4n@gmail.com>
2023-05-15 20:51:11 -0400

    pickup respawn

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -8,13 +8,41 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
+        [SerializeField] private float respawnTime= 5f;
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
+        }
+
+        IEnumerator HideForSeconds(float seconds)
+        {
+            HidePickup();
+            yield return new WaitForSeconds(seconds);
+            ShowPickup();
+        }
+
+        void HidePickup()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+
+            GetComponent<SphereCollider>().enabled = false;
+        }
+        
+        void ShowPickup()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+
+            GetComponent<SphereCollider>().enabled = true;
         }
     }
 }
